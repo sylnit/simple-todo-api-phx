96b68c6bcb9193c904c5a1e6a3e4665a560f06bf
Sylnit <h1samuel@yahoo.com>
2022-08-16 07:37:47 -0400

    Update schema to process todos

--- lib/todo_list_web/api/schema.ex
+++ lib/todo_list_web/api/schema.ex
@@ -1,10 +1,29 @@
 defmodule TodoListWeb.Api.Schema do
   use Absinthe.Schema
 
+  object :todo_item do
+    #ID!
+    field :id, non_null(:id)
+    field :content, non_null(:string)
+
+    field :is_completed, non_null(:boolean) do
+      resolve( fn %{completed_at: completed_at}, _, _ ->
+        {:ok, !is_nil(completed_at)}
+      end)
+    end
+  end
+
   query do
     field :hello, :string do
       resolve( fn _, _ ->
         {:ok, "Hello world!"}
+      end)
+    end
+
+    # [TodoItem!]!
+    field :todo_items, non_null(list_of(:todo_item)) do
+      resolve( fn _, _ ->
+        {:ok, TodoList.Todos.list_items()}
       end)
     end
   end

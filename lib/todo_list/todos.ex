866c09538de1c729f9f7ffa6500f03d6ae63e21e
Sylnit <h1samuel@yahoo.com>
2022-08-16 09:09:42 -0400

    Fix naming inconsistency

--- lib/todo_list/todos.ex
+++ lib/todo_list/todos.ex
@@ -110,7 +110,7 @@ defmodule TodoList.Todos do
     update_item(item, %{completed_at: nil})
   end
 
-  def toggle_todo_item_by_id(todo_item_id) when is_binary(todo_item_id) or is_integer(todo_item_id) do
+  def toggle_item_by_id(todo_item_id) when is_binary(todo_item_id) or is_integer(todo_item_id) do
     case Repo.get(Item, todo_item_id) do
       nil ->
         nil
